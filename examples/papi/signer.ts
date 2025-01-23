abfd7e3d8a1e0191743256cd711f6f332141fffc
muddlebee <anweshknayak@gmail.com>
2025-01-23 19:33:59 +0530

    fix

--- examples/papi/signer.ts
+++ examples/papi/signer.ts
@@ -17,9 +17,11 @@ const bobKeyPair = derive("//Bob")
 const ALICE = ss58Address(aliceKeyPair.publicKey);
 // Or with specific network prefix (e.g., 0 for Polkadot)
 const ALICE_POLKADOT = ss58Address(aliceKeyPair.publicKey, 0);
-
 console.log('Alice address:', ALICE);
+console.log('Alice address:', ALICE_POLKADOT); // 15oF4uVJwmo4TdGW7VfQxNLavjCXviqxT9S1MgbjMNHr6Sp5
+
+
 const BOB = ss58Address(bobKeyPair.publicKey);
-const BOB_POLKADOT = ss58Address(bobKeyPair.publicKey,63);
+const BOB_HDX = ss58Address(bobKeyPair.publicKey,63);
 console.log('Bob address:', BOB);
-console.log('Bob address:', BOB_POLKADOT);
+console.log('Bob address:', BOB_HDX); //7Lpe5LRa2Ntx9KGDk77xzoBPYTCAvj7QqaBx4Nz2TFqL3sLw
