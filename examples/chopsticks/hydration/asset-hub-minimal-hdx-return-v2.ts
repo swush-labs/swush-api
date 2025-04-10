3241e36a07d15a6fdf3e25db7e5b49be0b202b50
muddlebee <anweshknayak@gmail.com>
2025-04-10 20:37:47 +0530

    init

--- examples/chopsticks/hydration/asset-hub-minimal-hdx-return-v2.ts
+++ examples/chopsticks/hydration/asset-hub-minimal-hdx-return-v2.ts
@@ -114,8 +114,7 @@ async function main() {
         if (initialBalance.data.free < TRANSFER_AMOUNT) {
             throw new Error(`Insufficient balance. Have ${dotBalance} DOT, trying to transfer ${Number(TRANSFER_AMOUNT) / 1e10} DOT`)
         }
-        const TXN_FEE1 = TRANSFER_AMOUNT / 50n
-        const TXN_FEE2 = TRANSFER_AMOUNT / 50n
+        const TXN_FEE1 = TRANSFER_AMOUNT / 5n
         const FINAL_TRANSFER_AMOUNT = TRANSFER_AMOUNT + TXN_FEE1*2n;
         // Create assets
         // DOT asset for withdrawal from Asset Hub
